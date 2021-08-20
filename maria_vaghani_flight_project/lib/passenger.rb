25c7d4f71fc10a8d4437a1f1c5875a4c7c01b55a
mariavaghani <maria.vaghani@gmail.com>
2021-08-20 15:13:48 -0400

    complete Flight project

---
+++ maria_vaghani_flight_project/lib/passenger.rb
@@ -0,0 +1,18 @@
+class Passenger
+  
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @flight_numbers = []
+  end
+
+  def has_flight?(flight_number)
+    return true if @flight_numbers.include?(flight_number.upcase)
+    false
+  end
+
+  def add_flight(flight_number)
+    @flight_numbers << flight_number.upcase if !has_flight?(flight_number)
+  end
+end
